30fc2e713880d97d7f2a4f678925e48557a8bab6
Mark <mark.litfin@yahoo.com>
2020-10-29 14:58:12 -0400

    moved build

---
+++ build/precache-manifest.646ef19d1fe39615d4ddd760a5fb489e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d760090a0030ea26389597169e4d925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8c5535a881fd025955",
+    "url": "/static/css/main.8826c91a.chunk.css"
+  },
+  {
+    "revision": "d5fb31855c8037626a7d",
+    "url": "/static/js/2.9ca71793.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ca71793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8c5535a881fd025955",
+    "url": "/static/js/main.282030c9.chunk.js"
+  },
+  {
+    "revision": "344031586a8bb0a4be51",
+    "url": "/static/js/runtime-main.8fdc2c09.js"
+  }
+]);
